9877db34bbbe2338d7f1277a9c48a37277f4fb2c
frame88 <fleuzzi97@gmail.com>
2021-12-03 15:18:32 +0100

    schema in italiano

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+Visualizzare in pagina 5 numeri casuali poi fateli sparire.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri 
+che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali 
+dei numeri da indovinare sono stati individuati.
+*/
+
+//creo un ciclo for che si ferma a 5, e crea 5 numeri random, pushandoli in un array
+//parte il timer di 30 secondi con la time-function di stamattina
+//al termine della time-function un altro for che si ferma a 5, e chiede 5 volte di inserire il numero, lo pusha in un nuovo array
+//faccio un terzo for che si ferma sempre a 5, e con un if confronta le stringhe dei due array, quelli uguali in un terzo array
+//restituisco un messaggio con terzoarray.length e elementi del terzo array
+
